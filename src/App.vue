2053f8df0b99acd0b8745af04d3aaa17a88c8a56
YannBRL <ryan.3feitosa@gmail.com>
2025-10-16 04:59:19 -0300

    Primeira Atividade para casa

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      novaTarefa: '',
+      tarefas: []
+    }
+  },
+  methods: {
+    adicionarTarefa() {
+      if (this.novaTarefa.trim() !== '') {
+        this.tarefas.push(this.novaTarefa)
+        this.novaTarefa = ''
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Lista de Tarefas</h1>
+    <input v-model="novaTarefa" placeholder="Nova tarefa" />
+    <button @click="adicionarTarefa">Adicionar</button>
+
+    <p>Total de tarefas: {{ tarefas.length }}</p>
+
+    <ul>
+      <li v-for="(tarefa, index) in tarefas" :key="index">
+        {{ tarefa }}
+      </li>
+    </ul>
+  </div>
+</template>
